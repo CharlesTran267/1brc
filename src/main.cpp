0ba444be22717895a4634395bbfd909ae41e19f6
Charles Tran <charlestran267@gmail.com>
2026-02-18 21:33:58 +0800

    v4: use faster hash and compare

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <string>
 #include <thread>
 #include <unordered_map>
 #include <absl/container/flat_hash_map.h>
@@ -11,18 +12,18 @@
 #include "file_reader.cpp"
 
 #ifndef DEBUG
-#define DEBUG
+#define DEBUG 1
 #endif
 
 // dataset size: 10^SIZE_EXP rows. Must match what gen.py produced.
-#define SIZE_EXP 8
+#define SIZE_EXP 9
 #define STR_(x) #x
 #define XSTR_(x) STR_(x)
 #define SIZE_TAG "1e" XSTR_(SIZE_EXP)
 #define USE_STL 0
 
 #ifndef NUM_THREADS
-#define NUM_THREADS 8
+#define NUM_THREADS 1
 #endif
 
 constexpr auto INPUT_FILE = "input/measurements_" SIZE_TAG ".txt";
@@ -31,56 +32,68 @@ constexpr auto GOLDEN_OUTPUT = "output/golden_output_" SIZE_TAG ".txt";
 constexpr size_t pow10(int e) { return e ? 10 * pow10(e - 1) : 1; }
 constexpr size_t INPUT_FILE_LENGTH = pow10(SIZE_EXP);
 
+#if USE_STL
+template <class K, class V>
+using Map = std::map<K, V>;
+template <class K, class V>
+using UMap = std::unordered_map<K, V>;
+#else
+template <class K, class V>
+using Map = absl::btree_map<K, V>;
+template <class K, class V>
+using UMap = absl::flat_hash_map<K, V>;
+#endif
+
+using sit = std::string_view::iterator;
+using psv = std::pair<sit, sit>;
+
+struct Timer {
+  Timer(const std::string& prefix = "")
+      : m_pre(prefix), start(std::chrono::high_resolution_clock::now()) {}
+  ~Timer() {
+    // count in milliseconds
+    auto end = std::chrono::high_resolution_clock::now();
+    double duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+            .count();
+    std::cout << m_pre << ':' << duration << " ms\n";
+  }
+
+ private:
+  std::string m_pre;
+  std::chrono::high_resolution_clock::time_point start;
+};
+
 class Solver {
   struct Stat {
     int16_t min;
     int16_t max;
     int32_t sum;
     uint16_t count;
+    std::string_view sv;
   };
 
-  using K = std::string_view;
-#if USE_STL
-  using Map = std::map<K, Stat>;
-  using UMap = std::unordered_map<K, Stat>;
-#else
-  using Map = absl::btree_map<K, Stat>;
-  using UMap = absl::flat_hash_map<K, Stat>;
-#endif
-  using It = std::string_view::iterator;
-
-  struct Timer {
-    Timer(const std::string& prefix = "")
-        : m_pre(prefix), start(std::chrono::high_resolution_clock::now()) {}
-    ~Timer() {
-      // count in milliseconds
-      auto end = std::chrono::high_resolution_clock::now();
-      double duration =
-          std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
-              .count();
-      std::cout << m_pre << ':' << duration << " ms\n";
-    }
-
-   private:
-    std::string m_pre;
-    std::chrono::high_resolution_clock::time_point start;
-  };
+  using K = uint64_t;
+  using SMap = Map<K, Stat>;
+  using SUMap = UMap<K, Stat>;
 
  public:
   Solver(const std::string& file_name) : m_reader(file_name) {
-    Timer timer("Total solve");
+#ifdef DEBUG
+    Timer timer("Total process");
+#endif
 
     if constexpr (NUM_THREADS == 1) {
       thread_task(0, {m_reader.content.begin(), m_reader.content.end()});
       return;
     }
 
-    std::array<std::pair<It, It>, NUM_THREADS> byte_marks{};
+    std::array<psv, NUM_THREADS> byte_marks{};
     std::size_t chunk = m_reader.size / NUM_THREADS;
     byte_marks.front().first = m_reader.content.begin();
     byte_marks.back().second = m_reader.content.end();
     for (std::size_t i = 1; i < NUM_THREADS; i++) {
-      It start_byte = byte_marks[i - 1].first + chunk + 1;
+      auto start_byte = byte_marks[i - 1].first + chunk + 1;
       while ((*start_byte) != '\n') start_byte++;
       byte_marks[i].first = start_byte + 1;
       byte_marks[i - 1].second = byte_marks[i].first - 1;
@@ -96,7 +109,7 @@ class Solver {
     }
   }
 
-  void thread_task(std::size_t idx, std::pair<It, It> bm) {
+  void thread_task(std::size_t idx, psv bm) {
 #ifdef DEBUG
     Timer timer("Time to process " + std::to_string(idx));
 #endif
@@ -114,7 +127,8 @@ class Solver {
       std::string_view val_str(deli_it + 1, end_it - deli_it - 1);
       int16_t val = from_chars_op(val_str.begin(), val_str.end());
 
-      auto it = m_maps[idx].find(name);
+      auto h = lazy_hash(name);
+      auto it = m_maps[idx].find(h);
       if (it != m_maps[idx].end()) {
         Stat& st = it->second;
         st.count++;
@@ -122,7 +136,7 @@ class Solver {
         st.max = std::max(st.max, val);
         st.min = std::min(st.min, val);
       } else {
-        m_maps[idx].emplace(name, Stat{val, val, val, 1});
+        m_maps[idx].emplace(h, Stat{val, val, val, 1, name});
       }
       end_it++;
       start_it = end_it;
@@ -130,25 +144,29 @@ class Solver {
   }
 
   void write(const std::string& output_file) {
+    Map<std::string_view, Stat> combined_map;
+    {
 #ifdef DEBUG
-    Timer timer("Write time");
+      Timer timer("Aggregated time");
 #endif
-
-    Map combined_map;
-    for (auto& m : m_maps) {
-      for (auto& [k, v] : m) {
-        if (!combined_map.count(k)) {
-          combined_map[k] = std::move(v);
-          continue;
+      for (auto& m : m_maps) {
+        for (auto& [h, v] : m) {
+          auto it = combined_map.find(v.sv);
+          if (it == combined_map.end()) {
+            combined_map[v.sv] = {v.min, v.max, v.sum, v.count, v.sv};
+            continue;
+          }
+          combined_map[v.sv].count += v.count;
+          combined_map[v.sv].sum += v.sum;
+          combined_map[v.sv].min = std::min(combined_map[v.sv].min, v.min);
+          combined_map[v.sv].max = std::max(combined_map[v.sv].max, v.max);
         }
-
-        combined_map[k].count += v.count;
-        combined_map[k].sum += v.sum;
-        combined_map[k].min = std::min(combined_map[k].min, v.min);
-        combined_map[k].max = std::max(combined_map[k].max, v.max);
       }
     }
 
+#ifdef DEBUG
+    Timer timer("Write time");
+#endif
     std::ofstream out(output_file);
     if (!out.is_open()) {
       throw std::runtime_error("Failed to open file");
@@ -169,17 +187,19 @@ class Solver {
 
  private:
   FileReader m_reader;
-  std::array<UMap, NUM_THREADS> m_maps;
+  std::array<SUMap, NUM_THREADS> m_maps;
+  std::array<UMap<uint64_t, std::string_view>, NUM_THREADS> m_name_maps;
   std::array<std::thread, NUM_THREADS> m_thread_pool;
 };
-
-void process_line(const std::string& line) {}
 
 int main(int argc, char* argv[]) {
   // first argument is number of threads
   try {
-    Solver solver(INPUT_FILE);
-    solver.write(OUTPUT_FILE);
+    {
+      Timer timer("Total time");
+      Solver solver(INPUT_FILE);
+      solver.write(OUTPUT_FILE);
+    }
     if (!compare(OUTPUT_FILE, GOLDEN_OUTPUT)) {
       std::cout << "OUTPUT does NOT match GOLDEN_OUTPUT\n";
     } else {
